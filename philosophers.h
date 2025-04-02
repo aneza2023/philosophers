f913e3f842d8e9b6a96714dd5d9cdb41e7e90d3b
aneza2023 <163205385+aneza2023@users.noreply.github.com>
2025-04-02 11:41:53 +0000

    created threads

--- philosophers.h
+++ philosophers.h
@@ -15,10 +15,10 @@ typedef struct input_values
     // optional nb of meals;
 }   t_val;
 
-typedef struct philo_threads
-{
-    int id;
-}   t_thread;
+// typedef struct philo_threads
+// {
+//     int id;
+// }   t_thread;
 
 
 //check
@@ -28,6 +28,6 @@ int ft_atoi(char *argv);
 //philo
 int putting_values(t_val *input, char *todie, char *toeat, char *tosleep);
 int creating_threads(t_val *input);
-void philosophers_routine(void *arg);
+void *philosophers_routine(void *arg);
 
 #endif
--- threads.c
+++ threads.c
@@ -5,18 +5,43 @@ void *philosophers_routine(void *arg)
     int id;
 
     id = *(int*)arg; // what kind of sorcery is this??
-    printf("Philosopher nb. %d is thinking", id);
+    printf("Philosopher nb. %d is thinking\n", id);
     return (NULL);
 }
 
 int creating_threads(t_val *input)
 {
-    int      id;
-    t_thread philo1;
+    int       i;
+    pthread_t *philo;
+    int       *id;
 
-    id = 3;
-    philo1 = malloc(sizeof(t_thread));
-    pthread_create(&philo1, NULL, philosophers_routine, &id);
-
+    i = 1;
+    philo = malloc(sizeof(pthread_t) * (input->philo) + 1);
+    id = malloc(sizeof(int) * (input->philo) + 1);
+    while (i <= input->philo)
+    {
+        id[i] = i;
+        pthread_create(&philo[i], NULL, philosophers_routine, &id[i]);
+        i++;
+    }
+    i = 1;
+    while (i <= input->philo)
+    {
+        pthread_join(philo[i], NULL);
+        i++;
+    }
     return (0);
 }
+
+// int         id;
+// int         id2;
+// pthread_t   philo1;
+// pthread_t   philo2;
+
+// id = 3;
+// id2 = 8;
+// pthread_create(&philo1, NULL, philosophers_routine, &id);
+// pthread_create(&philo2, NULL, philosophers_routine, &id2);
+
+// pthread_join(philo1, NULL);
+// pthread_join(philo2, NULL);
