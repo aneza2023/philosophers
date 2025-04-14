0c4bbe7a34329cb4b36a8e90e7147b0e228bcc4a
aneza2023 <163205385+aneza2023@users.noreply.github.com>
2025-04-14 10:22:00 +0000

    seg fault when checking for death

--- routines.c
+++ routines.c
@@ -12,32 +12,49 @@
 
 #include "philosophers.h"
 
+int phil_eating(t_philo *phil)
+{
+    pthread_mutex_lock(phil->lfork);
+    printf("%ld %d has taken left fork\n", getting_timestamp(phil->start), phil->id);
+    pthread_mutex_lock(phil->rfork);
+    printf("%ld %d has taken right fork\n", getting_timestamp(phil->start), phil->id);
+    printf("%ld %d is eating\n", getting_timestamp(phil->start), phil->id);
+    usleep(phil->input->to_eat * 1000);
+    phil->nb_of_meals++;
+    phil->last_meal = getting_timestamp(phil->start);
+    pthread_mutex_unlock(phil->lfork);
+    pthread_mutex_unlock(phil->rfork);
+    return (0);
+}
+
+int phil_sleeping(t_philo *phil)
+{
+    printf("%ld %d is sleeping\n", getting_timestamp(phil->start), phil->id);
+    usleep(phil->input->to_sleep * 1000);
+    return (0);
+}
+int phil_death(t_philo *phil)
+{
+    if (getting_timestamp(phil->start) >= (phil->last_meal + phil->input->to_die)) // observer thread t put in
+    {
+        phil->death = 1;
+        printf("DIED phil nb %d\n", phil->id);
+    }
+    return (0);
+}
+
 int continue_routine(t_philo *phil)
 {
     while (phil->input->opt_meals != -1 && phil->someone_died != 1 && (phil->nb_of_meals <= phil->input->opt_meals))
     {
         //delete later >> observer fix
-        if (getting_timestamp(phil->start) >= (phil->last_meal + phil->input->to_die)) // observer thread t put in
-        {
-            phil->death = 1;
-            printf("DIED phil nb %d\n", phil->id);
-            break ;
-        }
         //printf("\ndeatSTART>> %d\n", phil->start);
         // printf("\ndeatLASTMEAL>> %ld\n", phil->last_meal);
         // printf("\ndeatTODIE>> %d\n", phil->input->to_die);
-        pthread_mutex_lock(phil->lfork);
-        printf("%ld %d has taken left fork\n", getting_timestamp(phil->start), phil->id);
-        pthread_mutex_lock(phil->rfork);
-        printf("%ld %d has taken right fork\n", getting_timestamp(phil->start), phil->id);
-        printf("%ld %d is eating\n", getting_timestamp(phil->start), phil->id);
-        usleep(phil->input->to_eat * 1000);
-        phil->nb_of_meals++;
-        phil->last_meal = getting_timestamp(phil->start);
-        pthread_mutex_unlock(phil->lfork);
-        pthread_mutex_unlock(phil->rfork);
-        printf("%ld %d is sleeping\n", getting_timestamp(phil->start), phil->id);
-        usleep(phil->input->to_sleep * 1000);
+        phil_eating(phil);
+        phil_death(phil);
+        phil_sleeping(phil);
+        phil_death(phil);
         printf("%ld %d is thinking\n", getting_timestamp(phil->start), phil->id);
     }
     return (0);
