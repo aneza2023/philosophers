d86a6210b1d209e18830fe6f5f76c8fbb4db187c
aneza2023 <163205385+aneza2023@users.noreply.github.com>
2025-04-15 09:52:11 +0000

    death time fixed i guess

--- observer.c
+++ observer.c
@@ -16,31 +16,32 @@ void *observer_routine(void *arg)
 {
     int     i;
     int     k;
-    t_observer   observer;
+    t_observer   *observer;
 
     observer = arg;
     i = 0;
-    k = 0;
-    //  while 1
-    while (observer->philosophers[k] != NULL)
+    while (1)
     {
-        if (observer->philosophers[k]->death == 1)
+        k = 0;
+        while (observer->philosophers[k] != NULL)
         {
-            while (observer->philosophers[i] != NULL)
+            if (observer->philosophers[k]->death == 1)
             {
-                observer->philosophers[i]->someone_died = 1;
-                i++;
+                while (observer->philosophers[i] != NULL)
+                {
+                    observer->philosophers[i]->someone_died = 1;
+                    i++;
+                }
             }
+            k++;
         }
-        printf("PHILL %d MEALS %d", observer->philosophers[k]->id, observer->philosophers[k]->nb_of_meals);
-        k++;
     }
     return (0);
 }
 
 int creating_observer(t_philo **philosophers, pthread_t *philo)
 {
-    t_observer   observer;
+    t_observer   *observer;
     pthread_t    observthread;
 
     observer = malloc(sizeof(t_observer));
@@ -49,6 +50,6 @@ int creating_observer(t_philo **philosophers, pthread_t *philo)
     observer->philosophers = philosophers;
     observer->philothread = philo;
     pthread_create(&observthread, NULL, observer_routine, observer);
-    pthread_join(observer, NULL);
+    pthread_join(observthread, NULL);
     return (0);
 }
--- routines.c
+++ routines.c
@@ -21,7 +21,7 @@ int phil_eating(t_philo *phil)
     printf("%ld %d is eating\n", getting_timestamp(phil->start), phil->id);
     usleep(phil->input->to_eat * 1000);
     phil->nb_of_meals++;
-    phil->last_meal = getting_timestamp(phil->start);
+    phil->last_meal = getting_timestamp(phil->start) + phil->input->to_eat;
     pthread_mutex_unlock(phil->lfork);
     pthread_mutex_unlock(phil->rfork);
     return (0);
@@ -35,10 +35,10 @@ int phil_sleeping(t_philo *phil)
 }
 int phil_death(t_philo *phil)
 {
-    if (getting_timestamp(phil->start) >= (phil->last_meal + phil->input->to_die)) // observer thread t put in
+    if (getting_timestamp(phil->start) > (phil->last_meal + phil->input->to_die)) // observer thread t put in
     {
         phil->death = 1;
-        printf("DIED phil nb %d\n", phil->id);
+        printf("%ld %d died\n", getting_timestamp(phil->start), phil->id);
     }
     return (0);
 }
@@ -48,13 +48,16 @@ int continue_routine(t_philo *phil)
     while (phil->input->opt_meals != -1 && phil->death != 1 && (phil->nb_of_meals <= phil->input->opt_meals))
     {
         if (phil->someone_died == 1)
+        {
+            printf("SOMEONEDIED %d\n", phil->someone_died);
             break ;
-        printf("SOMEONEDIED %d\n", phil->someone_died);
+        }
         phil_eating(phil);
         phil_death(phil);
         phil_sleeping(phil);
         phil_death(phil);
-        printf("%ld %d is thinking\n", getting_timestamp(phil->start), phil->id);
+        if (phil->death != 1)
+            printf("%ld %d is thinking\n", getting_timestamp(phil->start), phil->id);
     }
     return (0);
 }
@@ -65,9 +68,13 @@ int start_with_even(t_philo *phil)
     {
         if (phil->id % 2 == 0)
         {
+            phil_death(phil);
             phil_eating(phil);
+            phil_death(phil);
             phil_sleeping(phil);
-            printf("%ld %d is thinking\n", getting_timestamp(phil->start), phil->id);
+            phil_death(phil);
+            if (phil->death != 1)
+                printf("%ld %d is thinking\n", getting_timestamp(phil->start), phil->id);
         }   
         else
         {
